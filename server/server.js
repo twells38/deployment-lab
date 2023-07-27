7005f3a577800b2aaf4ff9cee1efa6f1060e8566
sornya <sornyaa9@hotmail.com>
2023-07-27 14:54:15 -0400

    devops

--- server/server.js
+++ server/server.js
@@ -3,6 +3,17 @@ const app = express()
 
 app.use(express.static(`public`))
 app.use(express.static(`${__dirname}/public`))  // middleware just functions that run in between the request and the response.
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'a5e4a9c05d5e4ac094fd4e43c8033f12',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 
 app.listen(4000, () => console.log(`server running on 4000`))
 
